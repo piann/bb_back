4b7215994538460ad2dba6592e9c026e35d91e77
piann <shadow_hat@naver.com>
2020-07-22 11:21:12 +0900

    add mutation : signIn and allowAccount

---
+++ src/api/User/allowAccount/allowAccount.ts
@@ -0,0 +1,25 @@
+import {prisma} from "../../../../generated/prisma-client"
+
+export default{
+    Mutation:{
+        allowAccount: async(_, args,{request}) =>{
+            try{
+                // add routine for check root
+
+                const {userId} = args;
+                // change lock status & company           
+                await prisma.updateUser({data:{
+                    isLocked:false,
+                    reasonOfLock:{disconnect:true},
+                }, 
+                where:{id:userId}
+                });
+                
+                return true;
+            }catch(err){
+                console.log(err);
+                return false;
+            }
+        } 
+    }
+}
