035e936be1e2db5b09400bb8d96b926d913dea05
piann <shadow_hat@naver.com>
2020-07-30 15:29:52 +0900

    add column for User : nickName

--- src/api/User/registerUser/registerUser.ts
+++ src/api/User/registerUser/registerUser.ts
@@ -12,22 +12,38 @@ export default{
                 const {
                     lastName,
                     firstName,
+                    nickName,
                     password,
                     email,
                     phoneNumber,
                 } = args;
-                const checkUser:number = await prisma.user.count({where:{email}});
-                let existEmail:boolean;
-                if(checkUser>=1){
-                    existEmail=true;
-                }else{
-                    existEmail=false;
-                }
-                if(existEmail===true){
+                const isEmailExisting:boolean = (await prisma.user.count({
+                    where:{
+                        email
+                    }
+                }) >=1 );
+                
+                if(isEmailExisting===true){
                     // Duplicate account check
                     console.log("This e-mail already exists")
                     return false
-                } else{
+
+                }
+
+                const isNickNameExisting:boolean = (await prisma.user.count({
+                    where:{
+                        nickName
+                    }
+                }) >=1 );
+                
+                if(isNickNameExisting===true){
+                    // // Duplicate nickName check
+                    console.log("This nickName already exists")
+                    return false
+
+                }
+                
+                
                     // Create user with input information and hashed password.
                     const passwordHash:string = generateSaltedHash(password);
 
@@ -37,6 +53,7 @@ export default{
                         data: {
                             lastName,
                             firstName,
+                            nickName,
                             passwordHash,
                             email,
                             phoneNumber,
@@ -48,7 +65,7 @@ export default{
 
                     return true;
 
-                }
+                
 
             } catch (err){
                 console.log(err);
