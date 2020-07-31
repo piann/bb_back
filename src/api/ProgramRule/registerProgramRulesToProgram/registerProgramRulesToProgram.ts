ce8ab310536405289d55a64de8d649886c05b0b5
piann <shadow_hat@naver.com>
2020-07-31 21:13:33 +0900

    modify mutation : registerProgramRulesToProgram() now pass argument pIdList

---
+++ src/api/ProgramRule/registerProgramRulesToProgram/registerProgramRulesToProgram.ts
@@ -0,0 +1,31 @@
+import { PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export default{
+    Mutation:{
+        registerProgramRulesToProgram: async(_, args:any,{request}):Promise<boolean> =>{
+            try{
+                //// add routine for check root
+
+                const {bId, pIdList} = args;
+                for(const pId of pIdList){
+                    await prisma.programRuleConnBugBounty.create({
+                        data:{
+                            programRule:{
+                                connect:{id:pId}
+                            },
+                            bugBountyProgram:{
+                                connect:{id:bId}
+                            }
+                        }
+                    });
+                }
+                return true;
+            }catch(err){
+                console.log(err);
+                return false;
+            }
+        }
+    }
+}
