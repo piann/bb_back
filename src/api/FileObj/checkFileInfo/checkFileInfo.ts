13d37cec02ad5ebb70637c36866b5e6fb988b783
piann <shadow_hat@naver.com>
2020-08-24 15:43:51 +0900

    add query : checkFileInfo()

---
+++ src/api/FileObj/checkFileInfo/checkFileInfo.ts
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+interface checkFileInfoResponse{
+    ok:Boolean;
+    fileType:String|undefined|null;
+    fileName:String|undefined|null;
+}
+
+
+export default{
+    Query:{
+        checkFileInfo: async(_, args:any,{request }):Promise<checkFileInfoResponse> =>{
+            try{
+                const {
+                    fileId,
+                    //userId
+                } = args;
+
+
+                const fileObj = await prisma.fileObj.findOne({
+                    where:{
+                        id:fileId
+                    }
+                })
+
+                if(fileObj?.isPublic!==true){
+                    //// need to add logic for private file
+                }
+
+                return {
+                    ok:true,
+                    fileType:fileObj?.fileType,
+                    fileName:fileObj?.fileName
+                }
+
+
+            }catch(err){
+                console.log(err);
+                return {
+                    ok:false,
+                    fileType:null,
+                    fileName:null
+                }
+            }
+        }
+    }
+}
