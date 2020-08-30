ed90db8fdab33f5feb8606d41e02ed88bea3cfd4
piann <shadow_hat@naver.com>
2020-08-30 14:43:43 +0900

    add mutation : modifyProgress()

---
+++ src/api/Report/modifyProgress/modifyProgress.ts
@@ -0,0 +1,43 @@
+import { PrismaClient, Role } from "@prisma/client";
+import { isAuthenticated } from "../../../middleware";
+
+const prisma = new PrismaClient()
+
+
+export default{
+    Mutation:{
+        modifyProgress: async(_, args:any,{request}):Promise<boolean|null> =>{
+            try{
+                
+                const {
+                    rId,
+                    progressIdx,
+                } = args;
+                if(isAuthenticated(request)===false){
+                    return false;
+                }
+                const { user:{role}} = request;
+
+                // only admin can modify progress
+                console.log(role);////
+                if(role!==Role.ADMIN){
+                    return false;
+                }
+                await prisma.progressStatus.create({
+                    data:{
+                        report:{
+                            connect:{id:rId}
+                        },
+                        progressIdx
+                    }
+                });
+
+                return true;
+
+            }catch(err){
+                console.log(err);
+                return null;
+            }
+        }
+    }
+}
